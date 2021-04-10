9e1eec595ebaa69cddf9e028adb102e17c23ea44
LeeDongGeon <secmatth1996@gmail.com>
2021-04-10 16:24:19 +0900

    chore: Update example

--- example.py
+++ example.py
@@ -8,17 +8,20 @@ import FinanceHistoryDataReader.Indicator as Indicator
 apple_history = qh.get_history("AAPL", "2021", per=True, pbr=True)
 nasdaq_history = qh.get_history("IXIC", "2020")
 bond_u3y_history = qh.get_history('US3YT=X', "2020")
+wti_history = qh.get_history("CL", "2020")
 
 nasdaq_sma5 = Indicator.get_SMA(nasdaq_history, 5)
 nasdaq_sma20 = Indicator.get_SMA(nasdaq_history, 20)
 bond_u3y_sma5 = Indicator.get_SMA(bond_u3y_history, 5)
 bond_u3y_sma20 = Indicator.get_SMA(bond_u3y_history, 20)
+wti_sma5 = Indicator.get_SMA(wti_history, 5)
 
 qh.add_column_by_day(apple_history, "nasdaq_ma5", nasdaq_sma5)
 qh.add_column_by_day(apple_history, "nasdaq_ma20", nasdaq_sma20)
 qh.add_column_by_day(apple_history, "bond_u3y_ma5", bond_u3y_sma5)
 qh.add_column_by_day(apple_history, "bond_u3y_ma20", bond_u3y_sma20)
+qh.add_column_by_day(apple_history, "wti_ma5", wti_sma5)
 
-qh.save_as_csv(apple_history, "AAPL_2021_with_nasdaq_ma_and_bond.csv")
+qh.save_as_csv(apple_history, "AAPL_2021_with_nasdaq_ma_bond_wti.csv")
 
 print(apple_history)
